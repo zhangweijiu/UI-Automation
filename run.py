6b935fc7874ae4c287d049567a50ec312142c4f7
zhangweijiu <73270008+zhangweijiu@users.noreply.github.com>
2021-01-25 16:20:09 +0800

    Add files via upload

--- run.py
+++ run.py
@@ -1,16 +1,16 @@
-# from common import method
-# from test_data import test_data
-# from selenium import webdriver
-# driver = webdriver.Chrome()
-# driver.implicitly_wait(10)
-#
-# url = test_data.data["ur1"]
-# username = test_data.data.get("username")
-# password = test_data.data.get("password")
-# key = test_data.data.get("key")
-#
-# num = method.search_fun(driver, url, username, password, key)
-# if key in num:
-#     print("搜索用例通过!")
-# else:
-#     print("搜索失败! ")
+from common import method
+from test_data import test_data
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.implicitly_wait(10)
+
+url = test_data.data["ur1"]
+username = test_data.data.get("username")
+password = test_data.data.get("password")
+key = test_data.data.get("key")
+
+num = method.search_fun(driver, url, username, password, key)
+if key in num:
+    print("搜索用例通过!")
+else:
+    print("搜索失败! ")
