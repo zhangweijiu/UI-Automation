3fe052f18ab54d46fb7ff85858094d2cc3f136d4
zhangweijiu <73270008+zhangweijiu@users.noreply.github.com>
2021-01-25 16:30:51 +0800

    Add files via upload

--- run.py
+++ run.py
@@ -4,12 +4,12 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 driver.implicitly_wait(10)
 
-url = test_data.data["ur1"]
+url = test_data.data["url"]
 username = test_data.data.get("username")
 password = test_data.data.get("password")
 key = test_data.data.get("key")
 
-num = method.search_fun(driver, url, username, password, key)
+num = method.search_func(driver, url, username, password, key)
 if key in num:
     print("搜索用例通过!")
 else:
